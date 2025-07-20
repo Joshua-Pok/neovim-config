e721a40a6026d5ac9346d2785477918f7c0211b9
JoshuaPok <official.joshuapok@gmail.com>
2025-07-20 22:37:36 +0800

    updated lualine

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -1,7 +1,16 @@
-return  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup()
-    end,
-  }
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'powerline_dark',
+      },
+      sections = {
+        lualine_c = {
+          { 'filename', path = 2 },
+        },
+      },
+    }
+  end,
+}
