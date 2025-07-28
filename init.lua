5d1fbb10b1c1e921a5c77e09166afa82e7613ac3
Joshua-Pok <official.joshuapok@gmail.com>
2025-07-28 23:09:26 +0800

    update for errors

--- init.lua
+++ init.lua
@@ -21,9 +21,28 @@ end
 -- Add lazy to runtime path
 vim.opt.rtp:prepend(lazypath)
 
+-- Enable diagnostics globally
+vim.diagnostic.config {
+  virtual_text = {
+    -- Show all diagnostics as inline virtual text
+    source = 'always', -- Show source of diagnostic
+    prefix = '●', -- Customize the prefix for virtual text
+    format = function(diagnostic)
+      return string.format('%s: %s', diagnostic.source, diagnostic.message)
+    end,
+  },
+  signs = true, -- Show signs in the sign column
+  update_in_insert = true, -- Update diagnostics even in insert mode
+  severity_sort = true, -- Sort diagnostics by severity
+  float = {
+    source = 'always', -- Show source in floating windows
+    border = 'rounded', -- Optional: nicer borders for floating diagnostics
+  },
+}
+
 -- Setup lazy.nvim
 require('lazy').setup({
-    {import = 'plugins'},
+  { import = 'plugins' },
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
@@ -45,4 +64,5 @@ require('lazy').setup({
 })
 
 -- Load additional config
-require('config')
+require 'config'
+
