71c49c8dd05177c8dd078f9d741ae6dcffc2ea68
Joshua-Pok <official.joshuapok@gmail.com>
2025-08-11 21:25:48 +0800

    autocomplete/import

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -1,15 +1,20 @@
 return {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    {
       'rcarriga/nvim-notify',
+      config = function()
+        require('notify').setup {
+          background_colour = '#000000', -- solid black to stop the warning
+        }
+        vim.notify = require 'notify' -- hook into vim.notify
+      end,
     },
-  }
+  },
+}
+
