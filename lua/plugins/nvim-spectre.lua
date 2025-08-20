a2cfda355dc0dc9745551e9d0256d5bfaad5f44f
Joshua-Pok <official.joshuapok@gmail.com>
2025-08-20 21:29:55 +0800

    find and replace

---
+++ lua/plugins/nvim-spectre.lua
@@ -0,0 +1,71 @@
+return {
+  'nvim-pack/nvim-spectre',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('spectre').setup {
+      -- Minimal config to avoid conflicts
+      color_devicons = true,
+      open_cmd = 'vnew',
+      live_update = false,
+      find_engine = {
+        ['rg'] = {
+          cmd = 'rg',
+          args = {
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+          },
+        },
+      },
+      replace_engine = {
+        ['sed'] = {
+          cmd = 'sed',
+          args = nil,
+        },
+      },
+      default = {
+        find = {
+          cmd = 'rg',
+        },
+        replace = {
+          cmd = 'sed',
+        },
+      },
+    }
+  end,
+  keys = {
+    {
+      '<leader>S',
+      function()
+        require('spectre').toggle()
+      end,
+      desc = 'Toggle Spectre',
+    },
+    {
+      '<leader>sw',
+      function()
+        require('spectre').open_visual { select_word = true }
+      end,
+      desc = 'Search current word',
+    },
+    {
+      '<leader>sw',
+      function()
+        require('spectre').open_visual()
+      end,
+      mode = 'v',
+      desc = 'Search current word',
+    },
+    {
+      '<leader>sp',
+      function()
+        require('spectre').open_file_search { select_word = true }
+      end,
+      desc = 'Search on current file',
+    },
+  },
+}
