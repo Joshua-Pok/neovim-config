2668a4107aafa9c6033dfd477830492e3224e559
Joshua-Pok <official.joshuapok@gmail.com>
2025-08-26 17:39:24 +0800

    outline-nvim is the way bro

---
+++ lua/plugins/outline-nvim.lua
@@ -0,0 +1,15 @@
+return {
+  'hedyhli/outline.nvim',
+  config = function()
+    -- Example mapping to toggle outline
+    vim.keymap.set('n', '<leader>ae', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+    require('outline').setup {
+      -- Your setup opts here (leave empty to use defaults)
+      outline_window = {
+        position = 'left',
+        width = 20,
+      },
+    }
+  end,
+}
