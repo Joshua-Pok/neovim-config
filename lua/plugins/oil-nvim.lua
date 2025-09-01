651b5b2b7824264a5dfeb402636c6b010dbb0dbe
Joshua-Pok <official.joshuapok@gmail.com>
2025-09-01 10:12:47 +0800

    removed mini icons redundancy

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -4,7 +4,7 @@ return {
   ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { { 'nvim-tree/nvim-web-devicons', opts = {} } },
   keys = {
     { '<leader>o', '<cmd>Oil<cr>', desc = 'Open Oil file explorer' },
   },
