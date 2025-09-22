7e55bf742059d239b030cbc9bd1fa1213fa5bbca
Joshua-Pok <official.joshuapok@gmail.com>
2025-09-22 17:37:03 +0800

    cool cursorline 2

--- lua/plugins/modes.lua
+++ lua/plugins/modes.lua
@@ -1,19 +1,21 @@
--- In your lazy.nvim setup file (usually plugins.lua)
+-- plugins.lua (Lazy.nvim)
 return {
-  {
-    'echasnovski/mini.nvim', -- modes.nvim is part of the mini.nvim ecosystem
-    version = false, -- latest commit
-    config = function()
-      require('mini.cursorword').setup() -- optional: for cursorword highlighting
-      require('mini.animate').setup() -- optional: for smooth animations
-      -- For modes.nvim specifically:
-      require('modes').setup()
-    end,
-  },
   {
     'mvllow/modes.nvim',
     config = function()
-      require('modes').setup()
+      require('modes').setup {
+        colors = {
+          bg = '', -- Optional bg param, defaults to Normal hl group
+          copy = '#f5c359',
+          delete = '#c75c6a',
+          change = '#c75c6a', -- Optional param, defaults to delete
+          format = '#c79585',
+          insert = '#78ccc5',
+          replace = '#245361',
+          select = '#9745be', -- Optional param, defaults to visual
+          visual = '#9745be',
+        },
+      }
     end,
   },
 }
